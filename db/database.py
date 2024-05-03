de6c9b91f6014af1f574cfa896a30b11443b0f3f
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-05-03 14:00:56 -0600

    feat: database using table integration

--- db/database.py
+++ db/database.py
@@ -1,11 +1,5 @@
-"""
-@file: table.py
-@package: db
-@description: Central class that simulates HBase functionality, managing DDL and DML operations.
-
-@author: Samuel Chamalé
-@date: may 2024
-"""
+# Importamos la clase Table desde el archivo table.py
+from table import Table
 
 
 class Database:
@@ -17,7 +11,7 @@ class Database:
 
     def create_table(self, table_name, column_families) -> str:
         """
-        Create a new table in the data base
+        Create a new table in the database using the Table class to manage column families.
 
         Args:
             table_name (str): Name of the table
@@ -26,39 +20,37 @@ class Database:
         Returns:
             str: Message with the result of the operation
         """
-
         if table_name in self.tables:
             return f"Table {table_name} already exists"
 
-        self.tables[table_name] = column_families
-        return f"Table {table_name} created"
+        # Aquí se crea un objeto de la clase Table con las familias de columnas proporcionadas
+        self.tables[table_name] = Table(table_name, column_families)
+        return f"Table {table_name} created successfully"
 
     def list_tables(self) -> list:
         """
-        List all tables in the data base
+        List all tables in the database
 
         Returns:
-            list: List of tables
+            list: List of table names
         """
-
         return list(self.tables.keys())
 
     def delete_table(self, table_name) -> str:
         """
-        Delete a table from the data base
+        Delete a table from the database
 
         Args:
-            table_name (str): Name of the table
+            table_name (str): Name of the table to be deleted
 
         Returns:
             str: Message with the result of the operation
         """
-
         if table_name not in self.tables:
             return f"Table {table_name} does not exist"
 
         del self.tables[table_name]
-        return f"Table {table_name} deleted"
+        return f"Table {table_name} deleted successfully"
 
 
 # Just for testing the basic Database object functionality
