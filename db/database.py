66de586522f8b48a7d8230ba79338222526b001e
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-05-03 14:00:04 -0600

    feat: database and table

--- db/database.py
+++ db/database.py
@@ -6,3 +6,65 @@
 @author: Samuel Chamalé
 @date: may 2024
 """
+
+
+class Database:
+    def __init__(self) -> None:
+        """
+        Initialize the database. Here should be load the initial state of the tables from file if needed.
+        """
+        self.tables = {}
+
+    def create_table(self, table_name, column_families) -> str:
+        """
+        Create a new table in the data base
+
+        Args:
+            table_name (str): Name of the table
+            column_families (list): List of column families
+
+        Returns:
+            str: Message with the result of the operation
+        """
+
+        if table_name in self.tables:
+            return f"Table {table_name} already exists"
+
+        self.tables[table_name] = column_families
+        return f"Table {table_name} created"
+
+    def list_tables(self) -> list:
+        """
+        List all tables in the data base
+
+        Returns:
+            list: List of tables
+        """
+
+        return list(self.tables.keys())
+
+    def delete_table(self, table_name) -> str:
+        """
+        Delete a table from the data base
+
+        Args:
+            table_name (str): Name of the table
+
+        Returns:
+            str: Message with the result of the operation
+        """
+
+        if table_name not in self.tables:
+            return f"Table {table_name} does not exist"
+
+        del self.tables[table_name]
+        return f"Table {table_name} deleted"
+
+
+# Just for testing the basic Database object functionality
+if __name__ == "__main__":
+    db = Database()
+    print(db.create_table("Usuarios", ["Info", "Contacto"]))
+    print(db.list_tables())
+    print(db.delete_table("Usuarios"))
+    print(db.list_tables())
--- db/table.py
+++ db/table.py
@@ -6,3 +6,72 @@
 @author: Samuel Chamalé
 @date: may 2024
 """
+
+
+class Table:
+    def __init__(self,  table_name, column_families=None) -> None:
+        """
+        Initialize a new table
+
+        Args:
+            table_name (str): Name of the table
+            column_families (list): An optional names of the column families to initialize the table
+        """
+
+        self.table_name = table_name
+        self.column_families = {
+            cf: {} for cf in column_families
+        } if column_families else {}
+
+    def add_column_family(self, cf_name) -> str:
+        """
+        Add a new column family to the table
+
+        Args:
+            cf_name (str): Name of the column family
+
+          Returns:
+            str: Message with the result of the operation
+        """
+
+        if cf_name in self.column_families:
+            return f"Column family {cf_name} already exists"
+
+        self.column_families[cf_name] = {}
+        return f"Column family {cf_name} added"
+
+    def delete_column_family(self, cf_name) -> str:
+        """
+        Delete a column family from the table
+
+        Args:
+            cf_name (str): Name of the column family
+
+        Returns:
+            str: Message with the result of the operation
+        """
+
+        if cf_name not in self.column_families:
+            return f"Column family {cf_name} does not exist"
+
+        del self.column_families[cf_name]
+        return f"Column family {cf_name} deleted"
+
+    def list_column_families(self) -> list:
+        """
+        List all column families in the table
+
+        Returns:
+            list: List of column families
+        """
+
+        return list(self.column_families.keys())
+
+
+# Just for testing the basic Table object functionality
+if __name__ == "__main__":
+    table = Table("Usuarios", ["Info", "Contacto"])
+    print(table.add_column_family("DatosAdicionales"))
+    print(table.list_column_families())
+    print(table.delete_column_family("Contacto"))
+    print(table.list_column_families())
