e9c5a584f04063d627726716ac3eb03e9a5586da
JDgomezGloadi <87883400+JDgomez2002@users.noreply.github.com>
2024-05-17 23:31:48 -0600

    feat: delete and deleteall

--- ds/Table.py
+++ ds/Table.py
@@ -6,22 +6,80 @@ class Table:
         self.table_name = table_name
         self.column_families = column_families
         self.is_enabled = True
-        self.table_path = BASES_PATH + base_name + table_name + '.json'
+        self.table_path = BASES_PATH + base_name + "/" + table_name + '.json'
+        print("table_path:", self.table_path, end="\n")
         self.data = self.loadData(self.table_path)
         self.versions = versions
 
     def loadData(self, file_path):
         return loadJsonFile(file_path)
-    
-    def put(self, row_id, col_family, col_name , value):
-        
+
+    def put(self, row_id: str, col_family: str, col_name: str , value: str):
+
+        row_id = str(row_id)
+        col_family = str(col_family)
+        col_name = str(col_name)
+        value = str(value)
+
         if not col_family in self.column_families:
             return False, "Column family not found in table"
 
-        self.data[row_id] = {}
-        self.data[row_id][col_family][col_name][value] = value
+        if row_id not in self.data:
+            self.data[row_id] = {}
 
-        return updateJsonFile(self.table_path, self.data)
+        if col_family not in self.data[row_id]:
+            self.data[row_id][col_family] = {}
 
-    # def update():
-        # 
+        if col_name not in self.data[row_id][col_family]:
+            self.data[row_id][col_family][col_name] = {}
+
+        self.data[row_id][col_family][col_name] = value
+
+        return updateJsonFile(self.table_path, self.data), "Data inserted successfully"
+
+    # ‘<table name>’, ‘<row>’, ‘<column name >’, ‘<time stamp>’
+    def delete(self, row_id: str, col_family: str, col_name: str , version: str):
+
+        row_id = str(row_id)
+        col_family = str(col_family)
+        col_name = str(col_name)
+
+        try:
+            version = int(version)
+        except:
+            return False, "Version should be an integer"
+
+        if not col_family in self.column_families:
+            return False, "Column family not found in table"
+
+        if row_id not in self.data:
+            return False, "Row not found in table"
+
+        if col_family not in self.data[row_id]:
+            return False, "Column family not found in row"
+
+        if col_name not in self.data[row_id][col_family]:
+            return False, "Column name not found in column family"
+
+        del self.data[row_id][col_family][col_name]
+
+        return updateJsonFile(self.table_path, self.data), "Data deleted successfully"
+    
+    def deleteall(self, row_id: str, col_family: str):
+
+        row_id = str(row_id)
+        col_family = str(col_family)
+
+        if not col_family in self.column_families:
+            return False, "Column family not found in table"
+
+        if row_id not in self.data:
+            return False, "Row not found in table"
+
+        if col_family not in self.data[row_id]:
+            return False, "Column family not found in row"
+
+        del self.data[row_id][col_family]
+
+        return updateJsonFile(self.table_path, self.data), "Data deleted successfully"
+
