84beca64d930dbe0f00c0953dc3c6d7410cf5273
JDgomezGloadi <jdev@gloadi.com>
2024-05-22 23:21:42 -0600

    feat: test db

--- generator/users.py
+++ generator/users.py
@@ -9,7 +9,7 @@ with open('./generator/users.txt', 'w') as file:
     for i in range(1, 101):
         row_id = i
         dpi = ''.join([str(fake.random_digit_not_null()) for _ in range(10)])
-        line = f"TableA {row_id} personal dpi {dpi}\n"
+        line = f"put TableA {row_id} personal dpi {dpi}\n"
         file.write(line)
 
 print("Data has been written to users.txt")
