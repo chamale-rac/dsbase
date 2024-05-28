1eb50fc86f00b45d9e75d16c3243645e7deeddcf
chamale-rac <samuel.chamale23@gmail.com>
2024-05-28 13:20:14 -0600

    feat: commands execution time using decorator

--- dsbase.py
+++ dsbase.py
@@ -2,6 +2,18 @@ import cmd
 import argparse
 from ds.Database import Database
 from ds.utils import printDict
+import time
+
+
+def timing(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} executed in {elapsed_time:.4f} seconds")
+        return result
+    return wrapper
 
 
 class DSBase(cmd.Cmd):
@@ -24,30 +36,30 @@ class DSBase(cmd.Cmd):
         "Clear the screen."
         print("\033[H\033[J")
 
-    # def do_help(self, arg): This is a built-in command
-
     #############################
     ###   General Commands    ###
     #############################
 
+    @timing
     def do_status(self, arg):
         "Get the status of the database."
         print(self.database.get_status())
 
+    @timing
     def do_version(self, arg):
         "Get the version of the database."
         print(self.database.get_version())
 
+    @timing
     def do_whoami(self, arg):
         "Get the user of the database."
         print(self.database.get_whoami())
-
-    # TODO: consider adding a do_info for obtaining metadata.
 
     #############################
     ###      DDL Commands     ###
     #############################
 
+    @timing
     def do_create_table(self, arg):
         "Create a table: create_table <table_name> <max_versions> <is_enabled: true> <column_family_names separated by space>"
         args = arg.split()
@@ -71,6 +83,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {table_name} already exists.")
 
+    @timing
     def do_list(self, arg):
         "List all tables in the database."
         tables = self.database.list_tables()
@@ -78,6 +91,7 @@ class DSBase(cmd.Cmd):
         for table in tables:
             print(table)
 
+    @timing
     def do_disable(self, arg):
         "Disable a table: disable <table_name>"
         res = self.database.disable_table(arg)
@@ -86,6 +100,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} not found.")
 
+    @timing
     def do_enable(self, arg):
         "Enable a table: enable <table_name>"
         res = self.database.enable_table(arg)
@@ -94,6 +109,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} not found.")
 
+    @timing
     def do_is_enabled(self, arg):
         "Check if a table is enabled: is_enabled <table_name>"
         res = self.database.is_enabled(arg)
@@ -102,6 +118,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} is disabled.")
 
+    @timing
     def do_describe(self, arg):
         "Describe the structure of a table: describe <table_name>"
         res = self.database.describe_table(arg)
@@ -111,6 +128,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} not found.")
 
+    @timing
     def do_exists(self, arg):
         "Check if a table exists: exist <table_name>"
         res = self.database.table_exists(arg)
@@ -119,6 +137,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} does not exist.")
 
+    @timing
     def do_drop(self, arg):
         "Drop a table: drop <table_name>"
         res = self.database.drop_table(arg)
@@ -127,6 +146,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Table {arg} not found.")
 
+    @timing
     def do_drop_all(self, arg):
         "Drop all tables in the database."
         status, message = self.database.drop_all_tables()
@@ -135,6 +155,7 @@ class DSBase(cmd.Cmd):
         else:
             print("Error dropping tables:", message)
 
+    @timing
     def do_alter(self, arg):
         "Alter a table: alter <table_name> <flag> <value>\nFlags: DELETE, RENAME, ADD\nIMPORTANT: Use ':' for old_col: new_col for RENAME flag."
         args = arg.split()
@@ -143,7 +164,7 @@ class DSBase(cmd.Cmd):
             return
 
         table_name, flag, value = args
-        success, message = self.alter_table(table_name, flag, value)
+        success, message = self.database.alter_table(table_name, flag, value)
         if success:
             print("Table altered successfully.")
         else:
@@ -153,6 +174,7 @@ class DSBase(cmd.Cmd):
     ###      DML Commands     ###
     #############################
 
+    @timing
     def do_put(self, arg):
         "Put a value in a table: put <table_name> <row_id> <column_family> <column_qualifier> <value>"
         args = arg.split()
@@ -171,6 +193,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Error: {message}")
 
+    @timing
     def do_get(self, arg):
         "Get a value from a table: get <table_name> <row_id>"
         args = arg.split()
@@ -187,6 +210,7 @@ class DSBase(cmd.Cmd):
         else:
             print(data)
 
+    @timing
     def do_scan(self, arg):
         "Scan a table: scan <table_name>"
         status, data = self.database.scan(arg)
@@ -197,6 +221,7 @@ class DSBase(cmd.Cmd):
         else:
             print(data)
 
+    @timing
     def do_delete(self, arg):
         "Delete a value from a table: delete <table_name> <row_id> <column_family> <column_qualifier> <version>"
         args = arg.split()
@@ -215,6 +240,7 @@ class DSBase(cmd.Cmd):
         else:
             print(f"Error: {message}")
 
+    @timing
     def do_delete_all(self, arg):
         "Delete all values of a row from a table: delete_all <table_name> <row_id>"
         args = arg.split()
@@ -226,11 +252,11 @@ class DSBase(cmd.Cmd):
         status, message = self.database.delete_all(table_name, row_id)
 
         if status:
-            print(
-                f"Deleted all values of row {row_id} in table {table_name}.")
+            print(f"Deleted all values of row {row_id} in table {table_name}.")
         else:
             print(f"Error: {message}")
 
+    @timing
     def do_count(self, arg):
         "Count the number of rows in a table: count <table_name>"
         args = arg.split()
@@ -246,6 +272,7 @@ class DSBase(cmd.Cmd):
         else:
             print(count)
 
+    @timing
     def do_truncate(self, arg):
         "Truncate a table: truncate <table_name>"
         status, message = self.database.truncate(arg)
